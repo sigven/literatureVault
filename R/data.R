c08f21d2e244642dec92d03ba4858ca4757e7d39
Sigve Nakken <sigven@ifi.uio.no>
2023-04-16 13:54:44 -0400

    create data export

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Vault of literature references for precision oncology
+#'
+#' A small dataset with identifiers and links to literature references
+#' of relevance for precision cancer medicine (biomarkers etc.)
+#'
+#' @format \bold{litvault} - A data frame with 14,640 rows and 3 columns:
+#' \itemize{
+#'   \item{source_id} - {source identifier (e.g. PMID)}
+#'   \item{source} - {type of source (PubMed, clinicaltrials.gov, FDA etc)}
+#'   \item{source_entity} - {gwas or biomarker}
+#'   \item{name} - {name of reference}
+#'   \item{link} - {HTML link of reference}
+#' }
+#'
+"litvault"
