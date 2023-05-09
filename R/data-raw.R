8fb5f37956c52c0140d451d6a25845299f68f9ec
Sigve Nakken <sigven@ifi.uio.no>
2023-05-09 12:12:36 +0200

    updated vault

--- R/data-raw.R
+++ R/data-raw.R
@@ -1,16 +1,16 @@
-# source_ids <- as.data.frame(
-#  readr::read_csv("data-raw/source_data.csv",
-#                  show_col_types = F)
-# )
-#
-# missing <- source_ids |>
-#   dplyr::anti_join(literatureVault::vault, by = "source_id")
-#
-# add_to_vault <- literatureVault:::get_literature(
-#  literature_df = missing, chunk_size = 250
-# )
-#
-# vault <- literatureVault::vault |>
-#   dplyr::bind_rows(add_to_vault)
-#
-# usethis::use_data(vault, overwrite = T)
+source_ids <- as.data.frame(
+ readr::read_csv("data-raw/source_data.csv",
+                 show_col_types = F)
+)
+
+missing <- source_ids |>
+  dplyr::anti_join(literatureVault::vault, by = "source_id")
+
+add_to_vault <- literatureVault:::get_literature(
+ literature_df = missing, chunk_size = 250
+)
+
+vault <- literatureVault::vault |>
+  dplyr::bind_rows(add_to_vault)
+
+usethis::use_data(vault, overwrite = T)
