9cc1fac6fc1cd050e9acb65fcff9aa7357f3b514
Sigve Nakken <sigven@ifi.uio.no>
2023-11-30 16:42:49 +0100

    added citations from Mitelmandb

--- R/data-raw.R
+++ R/data-raw.R
@@ -1,16 +1,16 @@
-# source_ids <- as.data.frame(
-#  readr::read_csv("data-raw/source_data.csv",
-#                  show_col_types = F)
-# )
+source_ids <- as.data.frame(
+ readr::read_csv("data-raw/source_data.csv",
+                 show_col_types = F)
+)
+
+missing <- source_ids |>
+  dplyr::anti_join(literatureVault::vault, by = "source_id")
+
+add_to_vault <- literatureVault:::get_literature(
+ literature_df = missing, chunk_size = 240
+)
 #
-# missing <- source_ids |>
-#   dplyr::anti_join(literatureVault::vault, by = "source_id")
-#
-# add_to_vault <- literatureVault:::get_literature(
-#  literature_df = missing, chunk_size = 240
-# )
-# #
-# vault <- literatureVault::vault |>
-#   dplyr::bind_rows(add_to_vault)
-#
-# usethis::use_data(vault, overwrite = T)
+vault <- literatureVault::vault |>
+  dplyr::bind_rows(add_to_vault)
+
+usethis::use_data(vault, overwrite = T)
